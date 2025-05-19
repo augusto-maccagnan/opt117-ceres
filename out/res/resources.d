48fd82de512dda26e123ec68e41ce4062f7d8666
Augusto <ammguto@gmail.com>
2025-05-19 19:27:29 -0300

    Removing compileld game files

--- out/res/resources.d
+++
@@ -1,8 +0,0 @@
-out/res/resources.o: res/resources.res \
-D:/GitHub/md_dev/evade5_items_hud/res/sprites/ship-bounds.png \
-D:/GitHub/md_dev/evade5_items_hud/res/sprites/ball.png \
-D:/GitHub/md_dev/evade5_items_hud/res/hud/hud.png \
-D:/GitHub/md_dev/evade5_items_hud/res/levels/level1/level1_tiles.png \
-D:/GitHub/md_dev/evade5_items_hud/res/backgrounds/level1_bg.png \
-D:/GitHub/md_dev/evade5_items_hud/res/levels/level1/level1_pal.pal \
-D:/GitHub/md_dev/evade5_items_hud/res/levels/level1/level1_map.tmx
